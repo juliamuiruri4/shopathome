cba5a126582b24ff1ba5a64ddf45607db7037d0e
Julia Muiruri <julia.muiruri4@gmail.com>
2024-06-18 16:52:39 +0300

    added connection to postgresql db

--- api-express/app.js
+++ api-express/app.js
@@ -1,6 +1,3 @@
-// write an api for the following actions, referencing sample data from discount-data.js and product-data.js from shared folder
-// discounts get
-// products get, delete, post, put
 
 const express = require('express');
 const app = express();
@@ -8,10 +5,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const productData = require('./shared/product-data');
-const discountData = require('./shared/discount-data');
+// const productData = require('./shared/product-data');
+// const discountData = require('./shared/discount-data');
 
-// const db = require('.queries.js');
+const db = require('./queries.js');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,38 +18,44 @@ app.get('/', (req, res) => {
     }
 );
 
-app.get('/api/discounts', (req, res) => {
-    res.json(discountData.getDiscounts());
-    }
-);
+app.get('/api/discounts', db.getDiscounts);
+app.get('/api/products', db.getProducts);
+app.post('/api/products', db.addProduct);
+app.put('/api/products', db.updateProduct);
+app.delete('/api/products/:id', db.deleteProduct);
 
-app.get('/api/products', (req, res) => {
-    res.json(productData.getProducts());
-    }
-);
+// app.get('/api/discounts', (req, res) => {
+//     res.json(discountData.getDiscounts());
+//     }
+// );
 
-app.post('/api/products', (req, res) => {
-    const product = req.body;
-    res.json(productData.addProduct(product));
-    }
-);
+// app.get('/api/products', (req, res) => {
+//     res.json(productData.getProducts());
+//     }
+// );
 
-app.put('/api/products', (req, res) => {
-    const product = req.body;
-    res.json(productData.updateProduct(product));
-    }
-);
+// app.post('/api/products', (req, res) => {
+//     const product = req.body;
+//     res.json(productData.addProduct(product));
+//     }
+// );
 
-app.delete('/api/products/:id', (req, res) => {
-    const id = req.params.id;
-    res.json(productData.deleteProduct(id));
-    }
-);
+// app.put('/api/products', (req, res) => {
+//     const product = req.body;
+//     res.json(productData.updateProduct(product));
+//     }
+// );
 
-app.listen(7071, () => {
-    console.log('Server is running on port 7071');
-    }
-);
+// app.delete('/api/products/:id', (req, res) => {
+//     const id = req.params.id;
+//     res.json(productData.deleteProduct(id));
+//     }
+// );
 
+const listen = () => {
+    app.listen(7071, () => {
+        console.log('Server is running on port 7071');
+    });
+}
 
-
+db.initializeDatabase(listen)
---
+++ api-express/queries.js
@@ -0,0 +1,139 @@
+// postgresql database
+
+const Pool = require('pg').Pool
+require('dotenv').config()
+
+const connectionString = process.env.POSTGRESS_URL
+
+const pool = new Pool({
+    connectionString
+})
+
+const selectProducts = `SELECT EXISTS (
+SELECT * FROM pg_tables
+WHERE schemaname = 'public'
+AND tablename = 'products'
+);`
+
+const selectDiscounts = `SELECT EXISTS (
+    SELECT * FROM pg_tables
+    WHERE schemaname = 'public'
+    AND tablename = 'discounts'
+);`
+
+// products - id, name, description, quantity
+const createProducts = `CREATE TABLE products ( 
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    quantity INT NOT NULL
+);`
+
+// discounts - id, store, percentage, code
+const createDiscounts = `CREATE TABLE discounts (
+    id SERIAL PRIMARY KEY,
+    store VARCHAR(255) NOT NULL,
+    percentage INT NOT NULL,
+    code VARCHAR(255) NOT NULL
+);`
+
+const initializeDatabase = (callback) => {
+    pool.query(selectProducts, (error, results) => {
+        if (error) {
+            console.log(`Failed to create products table: ${error}`)
+            callback();
+            return;
+        }
+        if (!results.rows[0].exists) {
+            console.log('Creating products table')
+            pool.query(createProducts, (error, results) => {
+                if (error) {
+                    console.log(error)
+                    throw error
+                }
+            })
+        } else {
+            callback()
+        }
+
+    })
+    pool.query(selectDiscounts, (error, results) => {
+        if (error) {
+            throw error
+        }
+        if (!results.rows[0].exists) {
+            pool.query(createDiscounts, (error, results) => {
+                if (error) {
+                    throw error
+                }
+            })
+        }
+        callback()
+    })
+}
+
+// get all products
+const getProducts = (request, response) => {
+    pool.query('SELECT * FROM products', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+// get all discounts
+const getDiscounts = (request, response) => {
+    pool.query('SELECT * FROM discounts', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+// add a new product
+const addProduct = (request, response) => {
+    const { name, description, quantity } = request.body
+
+    pool.query('INSERT INTO products (name, description, quantity) VALUES ($1, $2, $3)', [name, description, quantity], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Product added with ID: ${results.insertId}`)
+    })
+}
+
+// update a product
+const updateProduct = (request, response) => {
+    const { id, name, description, quantity } = request.body
+
+    pool.query('UPDATE products SET name = $1, description = $2, quantity = $3 WHERE id = $4', [name, description, quantity, id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`Product modified with ID: ${id}`)
+    })
+}
+
+// delete a product
+const deleteProduct = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('DELETE FROM products WHERE ID = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`Product deleted with ID: ${id}`)
+    }
+    )
+}
+
+module.exports = {
+    initializeDatabase,
+    getProducts,
+    getDiscounts,
+    addProduct,
+    updateProduct,
+    deleteProduct
+}
